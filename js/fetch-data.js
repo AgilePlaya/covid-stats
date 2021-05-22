a37644bc21af132d8c0c4d0bbf541e49b05bef53
AgilePlaya <60707646+AgilePlaya@users.noreply.github.com>
2021-05-23 01:12:45 +0530

    Added bgColor change, button to change states

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,24 +1,14 @@
-// // function to handle success
-// function success() {
-//     var data = JSON.parse(this.responseText); //parse the string to JSON
-//     console.log(data);
-//     console.log(data.summary.total)
-// }
+var requestURL = 'https://api.rootnet.in/covid19-in/stats/latest';
 
-// // function to handle error
-// function error(err) {
-//     console.log('Request Failed', err); //error details will be in the "err" object
-// }
+var grim_color = '#0e113f'
+var cream_color = '#f7eace'
+var green_color = '#e1f7ce'
+var scream_color = '#f7cece'
 
-// var xhr = new XMLHttpRequest(); //invoke a new instance of the XMLHttpRequest
-// xhr.onload = success; // call success function if request is successful
-// xhr.onerror = error;  // call error function if request failed
-// xhr.open('GET', 'https://api.rootnet.in/covid19-in/stats/latest'); // open a GET request
-// xhr.send(); // send the request to the server.
+var ovr_state = 0
+var number_type = 0
 
-let requestURL = 'https://api.rootnet.in/covid19-in/stats/latest';
-
-let request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
@@ -31,10 +21,38 @@ request.onload = function() {
     populateNumber(jsonData);
 }
 
-function populateNumber(obj) {
-    const dataNumber = document.createElement('p');
-    elem = document.getElementById('counter');
-    dataNumber.textContent = obj['data']['summary']['total'];
-    counter.appendChild(dataNumber);
+function changeBgColor(newColor) {
+    const body = document.getElementById('body');
+    body.style.backgroundColor = newColor
 }
 
+function populateNumber(obj) {
+    elem = document.getElementById('counter_number');
+
+    if (number_type == 0) {
+        elem.innerText = obj['data']['summary']['total'];
+    }
+    else if (number_type == 1) {
+        elem.innerText = obj['data']['summary']['discharged'];
+    }
+    else if (number_type == 2) {
+        elem.innerText = obj['data']['summary']['deaths'];
+    }
+    number_type = (number_type + 1) % 3
+}
+
+function buttonClick() {
+    if(ovr_state == 0) {
+        populateNumber(request.response)
+        changeBgColor(green_color)
+    }
+    if(ovr_state == 1) {
+        populateNumber(request.response)
+        changeBgColor(scream_color)
+    }
+    if(ovr_state == 2) {
+        populateNumber(request.response)
+        changeBgColor(cream_color)
+    }
+    ovr_state = (ovr_state + 1) % 3
+};
